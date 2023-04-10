25a92324e175fe94d38a8b5e9b9f5f899c39ccdf
MatveyOleynikov <ochoba-1@mail.ru>
2023-04-10 03:49:25 +0300

    add cout vector

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+//вывод вектора
+template<typename T>
+ostream& operator<<(ostream& o, const vector<T> & a)
+{
+    for (size_t i = 0; i < a.size(); ++i){
+        o << a[i] << " ";
+    }
+    o << '\n';
+    return o;
+}
+
 int main(){
     
     return 0;
